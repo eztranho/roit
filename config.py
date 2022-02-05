477f7da5ddc6cf2b2276466e16bb5b8f9b9c930f
Tales de Assis Pedroso <eztranho@hotmail.com>
2022-02-05 09:40:25 -0300

    fix: troca https para http nas urls

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ arquivo de configuração do pipeline
 '''
 cada camada é implementada como uma pasta em DATA_LAKE_PATH
 '''
+DATA_LAKE_PATH = 'C:\\Users\\Tales\\Desktop\\data_lake'
+CAMADAS        = ('raw','standardized', 'conformed', 'aplicacao')
 
-CAMADAS = ('raw','standardized', 'conformed', 'aplicacao')
-DATA_LAKE_PATH = r'C:Users/Tales/Desktop/data_lake'
 
 #==============================================================================
 # EXTRACT
@@ -22,9 +22,9 @@ cada url aponta para a localização do recurso na web
 cada nome em NOMES diz como o arquivo vai ser chamado em raw
 '''
 
-URLS = ('https://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.EMPRECSV.zip',
-        'https://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.SOCIOCSV.zip',
-        'https://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.ESTABELE.zip')
+URLS = ('http://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.EMPRECSV.zip',
+        'http://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.SOCIOCSV.zip',
+        'http://200.152.38.155/CNPJ/K3241.K03200Y0.D20108.ESTABELE.zip')
 
 NOMES = ('empresa.zip',
          'socio.zip',

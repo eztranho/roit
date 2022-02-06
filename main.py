1323558e1b1d9f2456bc01a8609169c6dae5afa8
Tales de Assis Pedroso <eztranho@hotmail.com>
2022-02-06 16:38:12 -0300

    feat: cria a versao bebe de transform, sem alteracao do conteudo das colunas por enquanto

--- main.py
+++ main.py
@@ -2,28 +2,51 @@
 
 from data_lake import criar_camadas
 from extract import extrair_um, mock_extrair_um
-from transform import de_raw_para_standardized
-from config import DATA_LAKE_PATH, NOMES_RAW, NOMES_STANDARDIZED, URLS, CAMADAS
+from transform import (de_raw_para_standardized, de_standardized_para_conformed,
+                       de_csv_para_csv_transformado)
+from config import DATA_LAKE_PATH, NOMES, EXTENSOES, URLS, CAMADAS
 from os import sep
 
-def criar_caminhos_raw():
-    caminhos = [DATA_LAKE_PATH + sep + CAMADAS[0] + sep + nome for nome in NOMES_RAW]
-    return caminhos
+"!!! cf chdir"
+"!!! arquivos são substituídos em vez de acumulados com versoes diferentes no data lake"
 
-def criar_caminhos_standardized():
-    caminhos = [DATA_LAKE_PATH + sep + CAMADAS[1] + sep + nome for nome in NOMES_STANDARDIZED]
+def criar_caminhos(camada):
+    '''
+    assume que dentro de uma mesma camada, os arquivos terão sempre a mesma extensão
+    '''
+    enum_camada = [*enumerate(CAMADAS)]
+    enum_extensao = [*enumerate(EXTENSOES)]
+    
+    # map cria {'raw': 0, 'standardized': 1, 'conformed': 2, 'aplicacao': 3}
+    # se mudarem as camadas em config.py, esta parte se acerta sozinha
+    map_camada = {camada: index for index, camada in enum_camada}
+     
+    index = map_camada.get(camada)
+    
+    if index is None:
+        raise ValueError("input errado para camada: {camada}\nesperava um desses: {CAMADAS}")
+    else:
+        caminhos = [DATA_LAKE_PATH + sep + CAMADAS[index] + sep + nome for nome in NOMES] 
+                    
+    # adicionando a extensão em cada caminho
+    ext = enum_extensao[index][1]
+    caminhos = [caminho + ext for caminho in caminhos]
+    
     return caminhos
 
 if __name__ == '__main__':
-    
     # cria pastas para cada camada do data lake, se elas não existirem
     criar_camadas()
     
     # cria full path dos arquivos zip na camada raw e standardized do data lake
-    caminhos_raw = criar_caminhos_raw()
-    caminhos_stand = criar_caminhos_standardized()
+    caminhos_raw   = criar_caminhos('raw')
+    caminhos_stand = criar_caminhos('standardized')
+    caminhos_conformed = criar_caminhos('conformed')
     
-    for url, caminho_raw, caminho_stand in zip(URLS, caminhos_raw, caminhos_stand):
+    for url, caminho_raw, caminho_stand, caminho_conformed  in zip(URLS, 
+                                                                   caminhos_raw, 
+                                                                   caminhos_stand, 
+                                                                   caminhos_conformed):
         try:
             mock_extrair_um(url, caminho_raw) # !!! apenas para evitar downloads demorados
                                               # na fase de desenvolvimento
@@ -35,6 +58,10 @@ if __name__ == '__main__':
             de_raw_para_standardized(caminho_raw, caminho_stand)
         except:
             continue # a exceção já é printada pela função
+            
+        de_standardized_para_conformed(caminho_stand, caminho_conformed)
+        
+        de_csv_para_csv_transformado(caminho_conformed)
         
         print('load goes here')
     
--- transform.py
+++ transform.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from zipfile import ZipFile
-from os import path, chdir, remove
+from os import path, chdir, remove, sep
 from glob import glob
-from shutil import move
+from shutil import move, copyfile
+from pathlib import Path
+import dask.dataframe as dd
+
 '''
 
 '''
+#=====================================================================================
+# HELPER FUNCTIONS
+
+def get_nome_do_arquivo(caminho_conformed):
+    nome = Path(caminho_conformed).stem
+    return nome
 
 def listar_arquivos_em_raw(caminho_pasta):
     chdir(caminho_pasta)
@@ -19,6 +28,92 @@ def diff_entre_listas(lista_menor, lista_maior):
     
     diff = s2 - s1
     return list(diff)
+
+def deletar_csvs_que_nao_terminam_em_numero(pasta):
+    numeros_str = [*map(str, [*range(10)])] # ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+    chdir(pasta)
+    
+    todos_csvs = glob('*.csv')
+    
+    csvs_sem_numero = [get_nome_do_arquivo(um_csv) + '.csv' # pegue socio + '.csv
+                       for um_csv in todos_csvs             
+                       if get_nome_do_arquivo(um_csv)[-1] not in numeros_str] # se o último char não for um número
+    
+    [*map(remove, csvs_sem_numero)]
+
+#=====================================================================================
+# CLASSES
+# usar specification design pattern
+    
+class Arquivo():
+    '''
+    base class. tem 3 tipos: empresa, socio e estabelecimento
+    as colunas mudam, o dtype muda e processamento muda
+    getter e setter não mudam
+    '''
+    
+    def __init__(self, caminho_conformed):
+        self.caminho_conformed = caminho_conformed
+        
+    def _tirar_backslash(self):
+        '''
+        operacao I/O custosa por causa de um \ N
+        '''
+        with open(self.caminho_conformed, 'r') as f:
+            string = f.read()
+            string = string.replace('\\', '')
+            
+        with open(self.caminho_conformed, 'w') as f:
+            f.write(string)
+        
+    def set_df(self):
+        self._tirar_backslash() 
+        self.df = dd.read_csv(self.caminho_conformed, 
+                              sep = ';',
+                              header = 0,
+                              encoding = 'unicode_escape', # !!! gambiarra para um caractere esotérico que empresa.csv tem --> byte 0xc7. checar 
+                              dtype = str) # por simplicidade
+        self.df.columns = self.columns
+        
+        # !!! considerar interface fluida --> return self
+        
+    def get_df(self):
+        return self.df
+  
+class Empresa(Arquivo): # dask recomenda não estender dd.DataFrame, então df será atributo
+    columns = ['cnpj_basico', 'razao_social', 'natureza_juridica', 
+               'qualificacao_do_resp', 'capital_social', 'porte', 'ente federativo']
+    
+    def __init__(self, caminho_conformed):
+        super().__init__(caminho_conformed)
+        
+class Socio(Arquivo):
+    columns = ['cnpj_basico', 'id_socio', 'nome_ou_razao_social', 
+               'cnpj_ou_cpf', 'qualificacao_socio', 'data_entrada', 
+               'pais', 'cpf_representante_legal', 
+               'nome_representante', 'qualificacao_representante',
+               'faixa_etaria']
+    
+    def __init__(self, caminho_conformed):
+        super().__init__(caminho_conformed)
+        
+class Estabelecimento(Arquivo):
+    columns = ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'id_matriz_filial',
+               'nome_fantasia', 'sit_cadastral', 'data_sit_cadastral',
+               'motivo_sit_cadastral', 'nome_cidade_exterior', 'pais',
+               'data_inicio_atividade', 'cnae_fiscal_principal', 
+               'cnae_fiscal_secundario', 'tipo_logradouro',
+               'logradouro', 'numero', 'complemento', 'bairro',
+               'cep', 'uf', 'municipio', 'ddd_1', 'tel_1',
+               'ddd_2', 'tel_2', 'ddd_fax', 'tel_fax',
+               'email', 'sit_especial', 'data_sit_especial']
+    
+    def __init__(self, caminho_conformed):
+        super().__init__(caminho_conformed)
+
+#=====================================================================================
+# HIGH-LEVEL FUNCTION
     
 def de_raw_para_standardized(caminho_raw, caminho_stand):
     print(f'começando a extrair o zip em {caminho_raw}')
@@ -55,3 +150,39 @@ def de_raw_para_standardized(caminho_raw, caminho_stand):
     # mover o csv para pasta standardized
     move(arquivo_csv, caminho_stand)
     print(f'arquivo csv movido para {caminho_stand}')
+
+def de_standardized_para_conformed(caminho_stand, caminho_conformed):
+    print(f'copiando csv "as is" de {caminho_stand} para {caminho_conformed}')
+    copyfile(caminho_stand, caminho_conformed)
+
+def de_csv_para_csv_transformado(caminho_conformed):
+    print(f'começando a transformar o arquivo {caminho_conformed}')
+    
+    # nome_do_arquivo é socio, empresa ou estabelecimento
+    nome_do_arquivo = get_nome_do_arquivo(caminho_conformed)
+    
+    # usando o nome do arquivo, vamos instanciar a classe certa
+    mapping = dict(empresa = Empresa,
+                   socio   = Socio,
+                   estabelecimento = Estabelecimento)
+    
+    class_ = mapping.get(nome_do_arquivo) # pega a classe Socio, Empresa ou Estabelecimento 
+    obj = class_(caminho_conformed)       # instancia 
+    obj.set_df()                          # de fato cria o DataFrame
+    
+    # !!! transformações de coluna vêm aqui --> fazer primeiro a modelagem do banco de dados
+    # algo como obj.transform()
+    
+    df = obj.get_df()                     # pega a versão processada final apenas
+                                          # vai ter vários métodos private
+                                          
+    nome_final = path.dirname(caminho_conformed) + sep + nome_do_arquivo + '_*.csv'
+    # e.g. C:\\Users\\Tales\\Desktop\\data_lake\\conformed\\empresa_0.csv
+    
+    df.to_csv(nome_final, sep = ';', index = False)
+    
+    pasta = path.dirname(caminho_conformed)
+    deletar_csvs_que_nao_terminam_em_numero(pasta)
+                                          
+    return
+
